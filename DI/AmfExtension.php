366c31538b7fcce97d97306811e52fd44b618c4b
Jan Langer <jan.langer@gmail.com>
2013-09-27 18:16:18 +0200

    stub of nette compiler extension

---
+++ DI/AmfExtension.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Goodshape\Amf\DI;
+
+
+use Nette\DI\CompilerExtension;
+
+/**
+ * Nette compiler extension
+ *
+ * @TODO
+ * @author Jan Langer <jan.langer@goodshape.cz>
+ * @package Goodshape\Amf\DI
+ */
+class AmfExtension extends CompilerExtension {
+
+
+
+} 
