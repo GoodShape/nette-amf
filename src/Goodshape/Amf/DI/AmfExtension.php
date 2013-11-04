6286a14b309045458fc157f407456c5b51a68344
Jan Langer <jan.langer@gmail.com>
2013-11-04 17:13:23 +0100

    added Compiler extension

--- src/Goodshape/Amf/DI/AmfExtension.php
+++ src/Goodshape/Amf/DI/AmfExtension.php
@@ -8,12 +8,27 @@ use Nette\DI\CompilerExtension;
 /**
  * Nette compiler extension
  *
- * @TODO
  * @author Jan Langer <jan.langer@goodshape.cz>
  * @package Goodshape\Amf\DI
  */
 class AmfExtension extends CompilerExtension {
 
+    private $defaults = [
+        'requestNamespaces' => [],
+        'mappings' => [],
+    ];
+
+
+    public function loadConfiguration() {
+
+        $builder = $this->getContainerBuilder();
+        $config = $this->getConfig($this->defaults);
+
+        $manager = $builder->addDefinition($this->prefix('manager'))
+                           ->setClass('Goodshape\Amf\Application\Manager', [$config]
+            );
+
+    }
 
 
 } 
